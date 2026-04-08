8ee45fec73be4234e232dd5302580c0fd908972c
AngelicaSalazarDelgado <angelica.salazar.delgado@est.una.ac.cr>
2026-04-08 09:51:43 -0600

    Agregar nombre en página de inicio

--- src/pages/InicioPage.jsx
+++ src/pages/InicioPage.jsx
@@ -3,9 +3,12 @@ function InicioPage() {
   return (
     <div>
       <h1>Sistema de gestión</h1>
+
       <p>Bienvenido a la aplicación React conectada con la API en Express.</p>
+      <p>Desarrollado por: Angelica Salazar</p>
       <p>
-        Usa el menú para ingresar al módulo de usuarios o al módulo de productos.
+        Usa el menú para ingresar al módulo de usuarios o al módulo de
+        productos.
       </p>
     </div>
   );
